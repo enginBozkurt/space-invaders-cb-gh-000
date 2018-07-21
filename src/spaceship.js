6f8c38f4a70083e3ebb3b7600d9e4e46dac2198c
Engin Bozkurt <engin.bozkurt034@gmail.com>
2018-07-21 22:54:31 +0000

    automatically backed up by learn

--- src/spaceship.js
+++ src/spaceship.js
@@ -1,1 +1,19 @@
-class Spaceship {}
+class Spaceship {
+  constructor(name, crew, phasers, shields){
+    this.name = name;
+    this.crew = crew;
+    this.phasers = phasers;
+    this.shields = shields;
+    this.phasersCharge = 'uncharged';
+    this.cloaked =  false;
+    this.warpDrive = 'disengaged';
+    crew.length ? (this.docked = false) : (this.docked = true);
+    this.setCrewCurrentShip();
+  }
+  
+  setCrewCurrentShip() {
+    this.crew.forEach((crew) => {
+      crew.currentShip = this;
+    });
+  }
+}
